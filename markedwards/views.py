12c23f279f4b7471ea618800a77c54c000c0a419
Mark Edwards <medwards3@gmail.com>
2014-11-24 11:30:04 -0500

    changed to Google API v3

--- markedwards/views.py
+++ markedwards/views.py
@@ -9,38 +9,39 @@ from datetime import datetime
 from blog.models import Post
 
 def get_json():
-	feed = urllib.request.urlopen("https://www.google.com/calendar/feeds/41lp9k24ggd5e2u5no7tmpldlg%40group.calendar.google.com/public/full?orderby=starttime&sortorder=ascending&futureevents=true&alt=json").read()
+	google_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000z")
+	feed = urllib.request.urlopen("https://www.googleapis.com/calendar/v3/calendars/41lp9k24ggd5e2u5no7tmpldlg@group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMin={}&key=AIzaSyDeEyzvi8F9s3_442dfQ8FC-IYdDa7S3eU".format(google_date)).read()
 	return feed
 
 def get_past_json():
-	google_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-	feed = urllib.request.urlopen("https://www.google.com/calendar/feeds/41lp9k24ggd5e2u5no7tmpldlg%40group.calendar.google.com/public/full?orderby=starttime&sortorder=descending&start-max={}&alt=json".format(google_date)).read()
+	google_date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000z")
+	feed = urllib.request.urlopen("https://www.googleapis.com/calendar/v3/calendars/41lp9k24ggd5e2u5no7tmpldlg@group.calendar.google.com/events?singleEvents=true&orderBy=startTime&timeMax={}&key=AIzaSyDeEyzvi8F9s3_442dfQ8FC-IYdDa7S3eU".format(google_date)).read()
 	return feed
 
 def json_parse(feed):
 	parsed = json.loads(feed.decode("utf8"))
 	jlist = []
-	for item in parsed['feed']['entry']:
+	for item in parsed['items']:
 		jitem = {}
-		jitem['title'] = item['title']['$t']
-		jitem['description'] = item['content']['$t']
-		to_parse = item["gd$when"][0]['startTime']
+		jitem['title'] = item['summary']
+		jitem['description'] = item['description']
+		to_parse = item['start']['dateTime']
 		jitem['time'] = parse_year(to_parse)
-		jitem['where'] = item["gd$where"][0]['valueString']
+		jitem['where'] = item["location"]
 		jlist.append(jitem)
 	return jlist
 
 def json_parse_past(feed):
 	parsed = json.loads(feed.decode("utf8"))
 	jlist = []
-	for item in parsed['feed']['entry']:
+	for item in parsed['items']:
 		jitem = {}
-		jitem['title'] = item['title']['$t']
-		jitem['description'] = item['content']['$t']
-		to_parse = item["gd$when"][0]['startTime']
+		jitem['title'] = item['summary']
+		jitem['description'] = item['description']
+		to_parse = item['start']['dateTime']
 		jitem['time'] = parse_year(to_parse)
-		jitem['where'] = item["gd$where"][0]['valueString']
-		jlist.append(jitem)
+		jitem['where'] = item["location"]
+		jlist.insert(0,jitem)
 	return jlist
 
 def json_parse_one(feed):
@@ -57,7 +58,7 @@ def json_parse_one(feed):
 def parse_year(ystring):
 	# To discard unnecessary info at the end of the string (timezone)
 	ylist = ystring.split('+')
-	parsed = datetime.strptime(ylist[0], "%Y-%m-%dT%H:%M:%S.000")
+	parsed = datetime.strptime(ylist[0], "%Y-%m-%dT%H:%M:%S")
 	##parsed = datetime.strptime(ystring, "%Y-%m-%dT%H:%M:%S.000+02:00")
 	new_time = parsed.strftime("%A %B %d, %H:%M")
 	return new_time
